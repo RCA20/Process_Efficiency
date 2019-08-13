bbad60065377cec4aa9900f82c4385f6703fae8d
RCA20 <33772336+RCA20@users.noreply.github.com>
2019-08-13 11:10:06 -0600

    Update Object_Counter.ino

--- Object_Counter.ino
+++ Object_Counter.ino
@@ -7,15 +7,11 @@
  */
 
  
-//#include <Wire.h>
-//#include <LiquidCrystal_I2C.h>
 
-#include <LiquidCrystal.h>
+#include <LiquidCrystal_I2C.h>
 
-//LiquidCrystal_I2C lcd(0x3F, 16, 2);
 
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+LiquidCrystal_I2C lcd(0x27, 16, 2); //Adress = 0x27 for PCF8574T
 
 
 int upButton = 7;
