b959a2eccf1b0a8a6c937ce4496421bee6624a87
RCA20 <33772336+RCA20@users.noreply.github.com>
2019-08-27 15:56:45 -0600

    proximity sensor test
    Program for testing coupling of proximity sensor and sketch.

---
+++ ProximitiySensor_Test.ino
@@ -0,0 +1,25 @@
+
+const int Sensor = 10; //Sensor input
+int Value = 0;         //Actual value of the sensor
+int old_Value = 0;     //Old value of the sensor
+int State = 0;         // 1 = Object detected
+
+void setup() {
+  pinMode(Sensor, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  Value = digitalRead(Sensor); // read the sensor input and stores it in val.
+
+  //check if there was a state transition
+  if ((Value == 1) && (old_Value == 0)) {
+    Serial.println("Object Detected (1)");
+  } else
+  {
+    Serial.println("Objct not detected (0)");
+  }
+  old_Value = Value;
+
+  delay(1000);
+}
